6d657a283acfddf9dbffce4d89c0cd51dec5bfab
sgemini <s13268001833@gmail.com>
2018-10-05 21:28:40 +0800

    add animal show method

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -4,4 +4,9 @@ class AnimalsController < ApplicationController
     animals = Animal.all
     render status: 200, json: { animals: animals }
   end
+
+  def show
+    animal = Animal.find_by(id: params[:id])
+    render status: 200, json: { animal: animal }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :animals, only: [:index]
+  resources :animals, only: [:index, :show]
 end
